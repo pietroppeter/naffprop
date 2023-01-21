6f41cecce3876c90607eba281a595f2c81584745
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-01-21 13:16:50 +0100

    add print for similarity matrix

--- similarity.nim
+++ similarity.nim
@@ -21,8 +21,10 @@
   - initial row index for 4x4 matrix are: 0, 3, 5
   - initial row index for 5x5 matrix are: 0 (1 2 3), 4 (5 6), 7 (8), 9
   - initial row index for nxn matrix are: 0 (1 .. n -1), n, n + (n - 1), n + (n - 1) + (n - 2), ... (n-1)*n/2 - 1
-    - for row index i I have i-th row index is n*i-T(i)-1? (where T(i) is i+1-th triangular number 1, 3, 6, 10, ... = (i+1)(i+2)/2)
+    - for row index i I have i-th row index is n*i-T(i)? (where T(i) is i+1-th triangular number 1, 3, 6, 10, ... = (i+1)(i+2)/2)
 ]#
+import std / strformat
+
 type
   SimMatrix* = object
     n*: int
@@ -50,10 +52,19 @@ iterator pairs*(s: SimMatrix): (int, int) =
     for j in (i + 1) ..< s.n:
       yield (i, j)
 
+func `$`*(s: SimMatrix): string =
+  for i in 0 ..< s.n:
+    for j in 0 ..< s.n:
+      debugEcho (i, j)
+      debugEcho s[i, j]
+      result.add fmt"{s[i, j]:>6.2f}"
+    result.add '\n'
+
 when isMainModule:
-  let s = SimMatrix(n: 4, pref: 0.0, data: @[1.0, 2, 3, 4, 5])
+  let s = SimMatrix(n: 4, pref: 0.0, data: @[1.0, 2, 3, 4, 5, 6])
   for i, j in s:
     stdout.write s.index(i, j)
     stdout.write " "
     if j == s.n - 1:
       stdout.write '\n'
+  echo s
